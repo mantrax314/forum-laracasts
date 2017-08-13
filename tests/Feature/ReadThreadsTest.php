21ffb09dbcdaae4d688743382b66d882b8681432
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-13 16:35:10 +0000

    EP 3 Single thread create
    EP 3 Single thread create

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -7,29 +7,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
 
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
 
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_see_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
 
     }
 }
