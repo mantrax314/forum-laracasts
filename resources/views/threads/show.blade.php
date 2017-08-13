8e345cb347e2b1e8111df0138dc1784fd3260af1
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-13 19:05:12 +0000

    EP 4 Added Thread's Creator Name

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,11 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $thread->title }}</div>
+
+                    <div class="panel-heading">
+                        <a href="#">{{ $thread->creator->name }}</a> posted:
+                        {{ $thread->title }}
+                    </div>
 
                     <div class="panel-body">
                         {{ $thread->body }}
