4226ece1ea1a0d2ddcd2f8134750adf517db2854
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-13 16:46:19 +0000

    EP 3 Added Test a_user_can_read_replies_that_are_associated_with_a_thread

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -33,4 +33,14 @@ class ReadThreadsTest extends TestCase
 
     }
 
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+
+    }
 }
