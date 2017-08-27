092c820969c126c4b0f4d999fb5785f3bbcbb82f
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-27 16:43:57 +0000

    EP 4 Added store Replies Method

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
-    //
+    public function store(Thread $thread)
+    {
+        $thread->addReply(request([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
