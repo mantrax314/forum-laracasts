41f8c4ff13a994fa68e3d1093aaee30a7455e048
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-27 17:11:42 +0000

    EP 4 Updated ParticipateInForum Test

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -9,18 +9,20 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ParticipateInForumTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given we have an authenticated user
-        $user = factory('App/User')->create();
-        $this->be($user);
+        $this->be($user = factory('App\User')->create());
 
         // And an existing Thread
         $thread = factory('App\Thread')->create();
 
         // When the user adds a reply to the thread
         $reply = factory('App\Reply')->make();
-        $this->post('/threads/' . $thread->id . '/repies', $reply->toArray());
+        $this->post($thread->path() . '/replies', $reply->toArray());
 
         // Then the reply should be visible on the page
         $this->get($thread->path())
