63bccea7322d539f3ba69f404ef63c2f46a5e48c
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-27 17:53:45 +0000

    EP 5 Add reply in a Thread

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="padding-bottom: 100px;">
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,24 @@
             </div>
         </div>
 
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have Something to say?"
+                                      rows="5"></textarea>
+                        </div>
+                        <button type="Submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">
+                Please <a href="{{ route('login') }}">sign int</a> to participate in this discussion.
+            </p>
+        @endif
+
     </div>
 @endsection
