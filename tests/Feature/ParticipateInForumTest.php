3d16d999a488678325662831afec81dd80dd39e1
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-27 16:21:13 +0000

    EP 4 Fix Name ParticipateInForumTest

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -7,13 +7,23 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ParticipateInForum extends TestCase
+class ParticipateInForumTest extends TestCase
 {
     function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given we have an authenticated user
+        $user = factory('App/User')->create();
+        $this->be($user);
+
         // And an existing Thread
+        $thread = factory('App\Thread')->create();
+
         // When the user adds a reply to the thread
+        $reply = factory('App\Reply')->make();
+        $this->post('/threads/' . $thread->id . '/repies', $reply->toArray());
+
         // Then the reply should be visible on the page
+        $this->get($thread->path())
+            ->assertSee($reply->body);
     }
 }
