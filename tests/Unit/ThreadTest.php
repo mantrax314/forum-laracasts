18654e45238e9ba214a813303294743f616d0b3e
Daniel Saavedra <mantrax3.14@gmail.com>
2017-08-27 16:41:26 +0000

    EP 4 Added addReply Method and Unit Test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -10,14 +10,20 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $thread;
+
     //Sample to run whole test file    phpunit tests/Unit/ThreadTest.php
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
 
     /** @test */
     public function a_thread_has_replies()
     {
-        $thread = factory('App\Thread')->create();
-
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
 
     //Sample to run whole this test only phpunit --filter a_thread_has_a_creator
@@ -25,8 +31,18 @@ class ThreadTest extends TestCase
     /** @test */
     public function a_thread_has_a_creator()
     {
-        $thread = factory('App\Thread')->create();
 
-        $this->assertInstanceOf('App\User', $thread->creator);
+        $this->assertInstanceOf('App\User', $this->thread->creator);
+    }
+
+    /** @test */
+    public function a_trhead_can_add_a_reply()
+    {
+        $this->thread->addReply([
+            'body' => 'foobar',
+            'user_id' => 1
+        ]);
+
+        $this->assertCount(1, $this->thread->replies);
     }
 }
